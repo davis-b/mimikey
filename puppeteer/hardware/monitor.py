7b39b67acb976ac83a154e39485d3cecc3d9e9a0
Davis <obviouspy@gmail.com>
2022-04-27 14:11:46 -0400

    Work around differences in Windows and Linux keygrabbing: Allow windows to host properly
    Windows' KeyGrab (keywatch) class does not work with the 'call_after_release' flag right now.
    Thus, to prevent calling cycle_receiver() twice in a row, once for the global keypress down, and
    once for the hooked keyboard keyup, we make sure to only call the cycle_receiver() function when
    the hooked keyboard has pressed the key both down and up.

--- puppeteer/hardware/monitor.py
+++ puppeteer/hardware/monitor.py
@@ -18,6 +18,7 @@ class Monitor:
 		self.grabbed = ActiveGrabber(self._keyboard, self._mouse_button, self._mouse_movement)
 		self.global_hotkeys = PassiveGrabber()
 		self._set_global_hotkeys()
+		self._last_keypress = (None, None)
 
 	def deinit(self):
 		self.global_hotkeys.deinit()
@@ -39,12 +40,15 @@ class Monitor:
 	def _keyboard(self, key, keystate):
 		if key == self._keybinds['pause_toggle']:
 			if keystate == key_states['key_up']:
-				self._push_pause_toggle()
+				if self._last_keypress[0] == key and self._last_keypress[1] != keystate:
+					self._push_pause_toggle()
 		elif key == self._keybinds['cycle_receiver']:
 			if keystate == key_states['key_up']:
-				self._push_cycle_receiver()
+				if self._last_keypress[0] == key and self._last_keypress[1] != keystate:
+					self._push_cycle_receiver()
 		else:
 			self.inputs.put((CommandType.keyboard, (key, keystate)))
+		self._last_keypress = (key, keystate)
 
 	def _mouse_button(self, button, button_state):
 		self.inputs.put((CommandType.mouse_button, (button, button_state)))
